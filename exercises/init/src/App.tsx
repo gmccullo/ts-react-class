2efe51856d813988401f6efe847287262ca1434f
John Paxton <pax@speedingplanet.com>
2018-06-11 20:24:12 -0400

    Initial add

---
+++ exercises/init/src/App.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <main className="container">
+        <section className="row">
+        <header className="col">
+          <h2>Nothing here yet.</h2>
+          <span><small>More once we do some exercises.</small></span>
+        </header>
+        </section>
+      </main>
+    );
+  }
+}
